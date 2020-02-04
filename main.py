bcf6b0d3ddea2f95a00ad44656def1d35057e39a
Ben Homan <benjaminhoman10100@gmail.com>
2020-02-03 21:24:40 -0500

    added module to generate large random primes

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-import numpy as np
-
-"""
-    Use the Sieve of Eratosthenes to generate a list of
-    primes up to the limit.
-    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-
-    returns numpy list of primes
-"""
-def primesUpUntil(limit):
-    bits = np.ones(limit, dtype=bool)
-    bits[0], bits[1] = False, False
-    for i in range(2, limit):
-        if bits[i]:
-            bits[i*i::i] = False
-    return np.flatnonzero(bits)
+import primes
 
 if __name__ == "__main__":
     print("RSA implementation")
-    print(sieve_of_eratosthenes(100))
+    #print(
+    print(generate_prime_number())
