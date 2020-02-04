6427555ea498d384f27c879001bd6b5091141a80
Ben Homan <benjaminhoman10100@gmail.com>
2020-02-03 21:50:23 -0500

    generate public and private key pair

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import primes
+import primes, math
+
+def rsaGenerate(bits):
+    e = 65537 #fixed public exponent
+    p = None
+    q = None
+    lamb = None
+
+    while True:
+        p = primes.generatePrimeNumber(bits)
+        q = primes.generatePrimeNumber(bits)
+        if True: #TODO does the public/private pair have a link
+            break
 
 if __name__ == "__main__":
     print("RSA implementation")
